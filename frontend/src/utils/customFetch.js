f2ffed3c8d80b6467ef3fd33556478741210083f
julandrod <julian.andres.rodriguez@gmail.com>
2024-06-15 10:00:06 -0500

    change the env variable needed for production

--- frontend/src/utils/customFetch.js
+++ frontend/src/utils/customFetch.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-  baseURL: import.meta.env.VITE_LOCAL_URL,
+  baseURL: import.meta.env.VITE_BACK_URL,
 });
 
 export default customFetch;
