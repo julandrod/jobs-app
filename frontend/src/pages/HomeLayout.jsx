d2ea665cc833cdb658fbd0962bd8359aee4a24ee
julandrod <julian.andres.rodriguez@gmail.com>
2024-06-15 10:15:21 -0500

    add conditional navbar

--- frontend/src/pages/HomeLayout.jsx
+++ frontend/src/pages/HomeLayout.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../components";
+import { useSelector } from "react-redux";
+import { selectAuthState } from "../features/authSlice";
 
 const HomeLayout = () => {
+  const { userInfo } = useSelector(selectAuthState);
+
   return (
     <>
-      <Navbar />
+      {!userInfo && <Navbar />}
       <Outlet />
     </>
   );
